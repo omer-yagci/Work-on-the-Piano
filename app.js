212f5f89e800e55f1c78fed134b3006a2c11a918
khan <omeryagci3295@gmail.com>
2022-05-14 19:21:45 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const a = document.querySelector("#A");
+const b = document.querySelector("#B");
+const c = document.querySelector("#C");
+const d = document.querySelector("#D");
+const e = document.querySelector("#E");
+const f = document.querySelector("#F");
+const g = document.querySelector("#G");
+const h = document.querySelector("#H");
+const k = document.querySelector("#K");
+const l = document.querySelector("#L");
+const m = document.querySelector("#M");
+const n = document.querySelector("#N");
+const o = document.querySelector("#O");
+
+window.addEventListener("keydown", (event) => {
+  if (event.key == "a") {
+    a.play();
+  } else if (event.key == "b") {
+    b.play();
+  } else if (event.key == "b") {
+    b.play();
+  } else if (event.key == "c") {
+    c.play();
+  } else if (event.key == "d") {
+    d.play();
+  } else if (event.key == "e") {
+    e.play();
+  } else if (event.key == "f") {
+    f.play();
+  } else if (event.key == "g") {
+    g.play();
+  } else if (event.key == "h") {
+    h.play();
+  } else if (event.key == "k") {
+    k.play();
+  } else if (event.key == "l") {
+    l.play();
+  } else if (event.key == "m") {
+    m.play();
+  } else if (event.key == "n") {
+    n.play();
+  } else if (event.key == "o") {
+    o.play();
+  }
+});
